c51994e48770d50bc9769eef01c4d8c81d549612
binoctal <binoctal@gmail.com>
2026-06-14 22:16:47 +0800

    style(autotest): group local import in gate_adapter_test

--- internal/autotest/gate_adapter_test.go
+++ internal/autotest/gate_adapter_test.go
@@ -4,9 +4,10 @@ import (
 	"context"
 	"testing"
 
-	"github.com/binoctal/cerberus/internal/escalation"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/binoctal/cerberus/internal/escalation"
 )
 
 func TestEscalationGateAdapter_DelegatesToInner(t *testing.T) {
